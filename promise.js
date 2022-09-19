54117a7e31872f3286d76ed06e26b4d2cca6d0d1
Ngurah Pranajaya <pranajayapinatih@gmail.com>
2022-09-19 22:18:34 +0800

    parameter emosi 'marah' atau 'tidak marah'.

--- promise.js
+++ promise.js
@@ -1,7 +1,24 @@
 const { promiseTheaterIXX, promiseTheaterVGC } = require("./external.js");
 
 // TODO: Buat fungsi promiseOutput sesuai ketentuan readme
-const promiseOutput = null;
+const promiseOutput = async () => {
+  await new Promise((output) => setTimeout(output, 200));
+
+  return new Promise((resolve) => {
+    const emosi = [
+      {
+        hasil: 'marah',
+      },
+      {
+        hasil: 'tidak marah',
+      },
+    ];
+
+    return resolve(emosi);
+  });
+};
+
+
 
 module.exports = {
   promiseOutput,
